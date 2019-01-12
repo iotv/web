04b2e843a9fcfb96b985b4d34efb71d41ba81a56
Mark Chandler <mark@chanchan.io>
2019-01-12 14:42:27 -0500

    chore: Convert to ts and remove cra stuff

---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+export const App = () => (
+  <div>
+    <h1>iotv</h1>
+    Welcome to our site on the World Wide Web aka the "Information Superhighway"
+  </div>
+)
