b57f91af2abbaedb1a080da191ef6fdfdd15635d
David J. Felix <felix.davidj@gmail.com>
2019-01-26 21:37:32 -0500

    feat: write first test and make button respect click

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,18 +1,29 @@
-import React, {FunctionComponent, ReactNode} from 'react'
+import React, {FunctionComponent, ReactNode, SyntheticEvent} from 'react'
 export type ButtonProps = {
-  disabled: boolean
-  type: string
   children: ReactNode
+  disabled: boolean
+  onClick: () => void
+  type: string
 }
 
-export const Button: FunctionComponent<Partial<ButtonProps>> = props => (
-  <button
-    className={
-      'flex bg-blue-dark hover:bg-blue text-white font-bold py-2 px-4 rounded'
+export const Button: FunctionComponent<Partial<ButtonProps>> = props => {
+  function onClickHandler(event: SyntheticEvent) {
+    event.preventDefault()
+    if (props.onClick) {
+      props.onClick()
     }
-    disabled={props.disabled}
-    type={props.type ? props.type : 'submit'}
-  >
-    {props.children}
-  </button>
-)
+  }
+
+  return (
+    <button
+      className={
+        'flex bg-blue-dark hover:bg-blue text-white font-bold py-2 px-4 rounded'
+      }
+      disabled={props.disabled}
+      onClick={onClickHandler}
+      type={props.type ? props.type : 'submit'}
+    >
+      {props.children}
+    </button>
+  )
+}
