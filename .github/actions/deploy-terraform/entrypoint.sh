a906e1e5f02761edfaf13a0058a2049b5869dbb3
David J. Felix <felix.davidj@gmail.com>
2019-02-02 15:27:39 -0500

    fix: make terraform auto-approve

--- .github/actions/deploy-terraform/entrypoint.sh
+++ .github/actions/deploy-terraform/entrypoint.sh
@@ -3,12 +3,14 @@
 set -eu
 
 terraform init \
-    -backend-config="bucket=${TF_S3_BUCKET}" \
-    -backend-config="region=${AWS_DEFAULT_REGION}" \
-    -backend-config="key=develop/iotv-api.tfstate" \
-    -backend-config="encrypt=true" \
-    -backend-config="kms_key_id=${TF_KMS_KEY_ARN}" \
-    -backend-config="dynamodb_table=${TF_DYNAMODB_TABLE}" \
-    terraform/
+  -backend-config="bucket=${TF_S3_BUCKET}" \
+  -backend-config="region=${AWS_DEFAULT_REGION}" \
+  -backend-config="key=develop/iotv-api.tfstate" \
+  -backend-config="encrypt=true" \
+  -backend-config="kms_key_id=${TF_KMS_KEY_ARN}" \
+  -backend-config="dynamodb_table=${TF_DYNAMODB_TABLE}" \
+  terraform/
 
-terraform apply terraform/
+terraform apply \
+  -auto-approve
+  terraform/
