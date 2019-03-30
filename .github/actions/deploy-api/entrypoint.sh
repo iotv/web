ab979b2d9d42de1edf0c1b0d09d7aab507cf6f87
David J. Felix <felix.davidj@gmail.com>
2019-03-30 08:49:17 -0400

    fix(ci): read the output file for deploy

--- .github/actions/deploy-api/entrypoint.sh
+++ .github/actions/deploy-api/entrypoint.sh
@@ -7,4 +7,4 @@ export AWS_DEFAULT_OUTPUT=json
 
 cd packages/graphql-service/
 zip -r -j dist/graphql.zip dist/*
-aws s3api put-object --bucket `` --key "graphql/graphql.${GITHUB_SHA}.zip" --body dist/graphql.zip
+aws s3api put-object --bucket `jq -r .bucketName ../../pulumi/pulumi-0-code-deploy/dist/output.json` --key "graphql/graphql.${GITHUB_SHA}.zip" --body dist/graphql.zip
