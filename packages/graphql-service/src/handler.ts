90f96399e91a6a04fbdec498e0283e7c67ca293e
David J. Felix <felix.davidj@gmail.com>
2019-03-31 15:09:54 -0400

    fix: allow all headers passed cross origin

--- packages/graphql-service/src/handler.ts
+++ packages/graphql-service/src/handler.ts
@@ -77,7 +77,7 @@ export async function handleCorsPreflight(
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'OPTIONS, POST',
-      'Access-Control-Allow-Headers': 'authorization, content-type',
+      'Access-Control-Allow-Headers': Object.keys(event.headers).join(','),
     },
   }
 }
