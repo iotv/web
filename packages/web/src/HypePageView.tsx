6a3a13dfd1b8c785a925ac64a877465963278ca9
David J. Felix <felix.davidj@gmail.com>
2019-04-03 23:01:18 -0400

    feat: make it obvious email was submitted
    - fixes #13

--- packages/web/src/HypePageView.tsx
+++ packages/web/src/HypePageView.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from 'react'
+import React, {FunctionComponent, useState} from 'react'
 
 import {HypeMainHeader} from './components/HypeMainHeader'
 import {HypeSubHeader} from './components/HypeSubHeader'
@@ -23,6 +23,8 @@ const betaSignupSchema = Yup.object().shape({
 
 export const HypePageView: FunctionComponent = props => {
   const applyForBeta = useMutation(APPLY_FOR_BETA)
+  const [isApplicatioAccepted, setIsApplciationAccpeted] = useState(false)
+
   return (
     <div className={'flex flex-col min-h-screen'}>
       <div className={'bg-gray-800 h-12'}>
@@ -48,46 +50,51 @@ export const HypePageView: FunctionComponent = props => {
           <HypeSubHeader className={'text-center'}>
             Stream. Edit. Remix. Collab.
           </HypeSubHeader>
-          <Formik
-            initialValues={{email: ''}}
-            validationSchema={betaSignupSchema}
-            onSubmit={async ({email}, {setSubmitting, setFieldError}) => {
-              try {
-                setSubmitting(true)
-                await applyForBeta({variables: {email}})
-              } catch {
-                setFieldError('email', 'Email already registered')
-              } finally {
-                setSubmitting(false)
-              }
-            }}
-          >
-            {({isSubmitting, errors}) => (
-              <Form className={'flex flex-col'}>
-                {isSubmitting ? (
-                  <div className="text-sm text-gray-100">Submitting...</div>
-                ) : (
-                  ''
-                )}
-                <ErrorMessage name="email">
-                  {msg => <div className="text-sm text-gray-100">{msg}</div>}
-                </ErrorMessage>
-                <Field name="email">
-                  {(props: FieldProps) => (
-                    <Input
-                      className={`${getFormikClassName(props)} mb-2 ${
-                        errors.email ? '' : 'border-red border border-4'
-                      }`}
-                      {...props.field}
-                      placeholder="Email"
-                      type="email"
-                    />
+          {isApplicatioAccepted ? (
+            <div className="text-gray-100">See you soon!</div>
+          ) : (
+            <Formik
+              initialValues={{email: ''}}
+              validationSchema={betaSignupSchema}
+              onSubmit={async ({email}, {setSubmitting, setFieldError}) => {
+                try {
+                  setSubmitting(true)
+                  await applyForBeta({variables: {email}})
+                } catch {
+                  setFieldError('email', 'Email already registered')
+                } finally {
+                  setSubmitting(false)
+                  setIsApplciationAccpeted(true)
+                }
+              }}
+            >
+              {({isSubmitting, errors}) => (
+                <Form className={'flex flex-col'}>
+                  {isSubmitting ? (
+                    <div className="text-sm text-gray-100">Submitting...</div>
+                  ) : (
+                    ''
                   )}
-                </Field>
-                <Button>Sign up for the beta</Button>
-              </Form>
-            )}
-          </Formik>
+                  <ErrorMessage name="email">
+                    {msg => <div className="text-sm text-gray-100">{msg}</div>}
+                  </ErrorMessage>
+                  <Field name="email">
+                    {(props: FieldProps) => (
+                      <Input
+                        className={`${getFormikClassName(props)} mb-2 ${
+                          errors.email ? '' : 'border-red border border-4'
+                        }`}
+                        {...props.field}
+                        placeholder="Email"
+                        type="email"
+                      />
+                    )}
+                  </Field>
+                  <Button>Sign up for the beta</Button>
+                </Form>
+              )}
+            </Formik>
+          )}{' '}
         </div>
       </div>
     </div>
