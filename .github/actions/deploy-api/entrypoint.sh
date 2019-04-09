c566c04e47a7ebe91c7ef8329da5f78f15ae46b9
David J. Felix <felix.davidj@gmail.com>
2019-04-09 08:19:51 -0400

    fix: such a little change

--- .github/actions/deploy-api/entrypoint.sh
+++ .github/actions/deploy-api/entrypoint.sh
@@ -6,5 +6,5 @@ export AWS_DEFAULT_REGION=us-east-1
 export AWS_DEFAULT_OUTPUT=json
 
 cd packages/graphql-service/
-zip -r dist/graphql.zip ./
+zip -r dist/graphql.zip ./*
 aws s3api put-object --bucket `jq -r .bucketName ../../pulumi/pulumi-0-code-deploy/dist/output.json` --key "graphql/graphql.${GITHUB_SHA}.zip" --body dist/graphql.zip
