3f345db8e2e2cf4bdf370c5bacbd9dbf1f518d9a
David J. Felix <felix.davidj@gmail.com>
2019-04-12 20:46:30 -0400

    fix(ci): make sure we cd up

--- .github/actions/deploy-api/entrypoint.sh
+++ .github/actions/deploy-api/entrypoint.sh
@@ -9,6 +9,7 @@ cd packages/graphql-service/
 zip -r dist/graphql.zip ./*
 aws s3api put-object --bucket `jq -r .bucketName ../../pulumi/pulumi-0-code-deploy/dist/output.json` --key "graphql/graphql.${GITHUB_SHA}.zip" --body dist/graphql.zip
 
+cd ../../
 cd packages/password-service/
 zip dist/create-password-hash.zip dist/create-password-hash
 zip dist/validate-password-hash.zip dist/validate-password-hash
